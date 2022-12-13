0f7c54016eeacc20167a5a3570376d3dfba3adfa
Nadineja <88454381+Nadineja@users.noreply.github.com>
2022-12-13 00:01:50 -0500

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,26 +3,10 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
-     const handleSetBudget = (event) => {
-		dispatch({
-			type: 'SET_BUDGET',
-			payload: event.target.value,
-		});
-
+    
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
-            <input
-						required='required'
-						type='number'
-						id='budget'
-						value={budget}
-                        step='10'
-                        size = '6'
-                        style={{ marginLeft: '2rem' , size: 6}}
-						onChange={handleSetBudget}
-                        >
-						</input>
         </div>
     );
 };
